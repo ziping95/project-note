29d4a9b610deb5e339206a548887cd95ced4bb10
ziping95 <wangziping@whaty.com>
2020-05-03 11:12:44 +0800

    增加脚本命令

--- release.sh
+++ release.sh
@@ -1,6 +1,42 @@
 #!/bin/bash
 
+temp="vue-temp"
+currentFolder="project-note"
+branchName="release"
+
+
 cd ../
-mkdir vue-temp
-cd project-note
-mv node_module docs/.vuepress/dist/* -t ../vue-temp
+#文件夹不存在则创建
+if [ ! -d "./${temp}" ];then
+  mkdir ${temp}
+  else
+  echo "删除文件夹"
+  rm -rf ./${temp}
+  mkdir ${temp}
+fi
+
+cd ${currentFolder}
+cp node_module docs/.vuepress/dist/* -t ../${temp}
+
+
+commit=`git status -s`
+if [ ! -n "${commit}" ];then
+    echo "已全部提交，继续执行"
+else
+	echo "当前分支有未提交文件，请先提交后再执行脚本"
+	exit
+fi
+
+git checkout ${branchName}
+
+rm -rf !(.git|.gitignore) 
+mv ../${temp}/* ./
+nowData="`date +%Y-%m-%d`"
+echo ${nowData}
+#git add -A
+#git commit -m ${nowData}
+#git push 
+
+#git checkout master
+
+
