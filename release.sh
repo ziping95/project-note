576d1d0835f1ffece43c532511bedda42da7d640
ziping95 <wangziping@whaty.com>
2020-05-03 11:40:29 +0800

    fix

--- release.sh
+++ release.sh
@@ -4,19 +4,22 @@ temp="vue-temp"
 currentFolder="project-note"
 branchName="release"
 
+# -e遇到未正确执行的命令立刻退出
+# -u 遇到未定义的变量，报错（但是不会退出）
+set -e -u
 
 cd ../
 #文件夹不存在则创建
 if [ ! -d "./${temp}" ];then
+  echo "创建临时文件夹"
   mkdir ${temp}
   else
-  echo "删除文件夹"
-  rm -rf ./${temp}
-  mkdir ${temp}
+  echo "已存在临时文件夹，清空临时文件夹"
+  rm -rf ./${temp}/*
 fi
 
 cd ${currentFolder}
-cp -r node_modules/* docs/.vuepress/dist/* -t ../${temp}
+cp -r node_modules docs/.vuepress/dist/* -t ../${temp}
 
 commit=$(git status -s)
 if [ ! -n "${commit}" ];then
