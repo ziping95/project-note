d24fb1b52fd5df96c32eccccfbc8fb26729ab9dc
ziping95 <wangziping@whaty.com>
2020-05-03 13:05:42 +0800

    修改脚本

--- release.sh
+++ release.sh
@@ -1,46 +1,26 @@
 #!/bin/bash
 
-temp="vue-temp"
 currentFolder="project-note"
-branchName="release"
+targetDirName="release-note"
 
 # -e遇到未正确执行的命令立刻退出
 # -u 遇到未定义的变量，报错（但是不会退出）
 set -e -u
 
-cd ../
-#文件夹不存在则创建
-if [ ! -d "./${temp}" ];then
-  echo "创建临时文件夹"
-  mkdir ${temp}
-  else
-  echo "已存在临时文件夹，清空临时文件夹"
-  rm -rf ./${temp}/*
-fi
+cd ../${targetDirName}
+ls | grep -v ".git" | xargs rm -rf
 
-cd ${currentFolder}
-cp -r node_modules docs/.vuepress/dist -t ../${temp}
-mv ../${temp}/dist/* ../${temp}
-rm -rf ../${temp}/docs
+cd ../${currentFolder}
+cp -r node_modules docs/.vuepress/dist -t ../${targetDirName}
 
-commit=$(git status -s)
-if [ ! -n "${commit}" ];then
-    echo "已全部提交，继续执行"
-else
-	echo "当前分支有未提交文件，请先提交后再执行脚本"
-	exit
-fi
+cd ../${targetDirName}
+mv ./dist/* ./
+rm -rf ./dist
 
-git checkout ${branchName}
-ls | xargs rm -rf
-
-mv ../${temp}/* ./
 nowData="`date +%Y-%m-%d` 发布"
 echo ${nowData}
-git add -A
-git commit -m ${nowData}
+git add .
+git commit -m "${nowData}"
 git push
-
-git checkout master
 
 
